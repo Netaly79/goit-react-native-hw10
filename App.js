bfe796b3cbf650b53bd0eacb67f5b4beb88f3ebe
Karpenko Nataliia <48415878+Netaly79@users.noreply.github.com>
2024-11-19 20:54:19 +0100

    registration

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RegistrationScreen from './screens/RegistrationScreen';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <RegistrationScreen />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, // Растягивает SafeAreaView на всё пространство
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
